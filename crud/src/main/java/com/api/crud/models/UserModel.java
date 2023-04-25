409deddd4cf30ce0fadb716353d44d132deae618
DESKTOP-C9HHN7V\A8-PC00 <licencias@svalero.com>
2023-04-25 21:16:09 +0200

    crud

--- crud/src/main/java/com/api/crud/models/UserModel.java
+++ crud/src/main/java/com/api/crud/models/UserModel.java
@@ -1,2 +1,53 @@
-package com.api.crud.models;public class UserModel {
+package com.api.crud.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="usuarios")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private String correo;
+
+    @Column
+    private String contra;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
 }
