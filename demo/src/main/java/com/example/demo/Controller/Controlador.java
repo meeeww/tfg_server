c210a5bdb1d3ec2aeb863ce0eb31a298a5595fce
meeeww <thezasm03@gmail.com>
2023-05-02 16:29:22 +0200

    Añadido CORS

--- demo/src/main/java/com/example/demo/Controller/Controlador.java
+++ demo/src/main/java/com/example/demo/Controller/Controlador.java
@@ -17,36 +17,43 @@ public class Controlador {
     private CaracteristicasService impl;
 
     //-----------------------------------------CARACTERISTICAS-----------------------------------------\\
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/caracteristicas/consultar")
     public List<Caracteristicas> consultarCaracteristicasQuery(){
         return impl.ConsultarCaracteristicas();
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/caracteristicas/buscar")
     public List<Caracteristicas> buscarCaracteristicaQuery( @RequestParam(value = "id") Set<Integer> id){
         return impl.BuscarCaracteristica(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/caracteristicas/crear")
     public void crearCaracteristicaQuery(@RequestBody Caracteristicas caracteristica){
         impl.CrearCaracteristica(caracteristica.getNombreCaracteristica(), caracteristica.getValorCaracteristica());
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PutMapping("/caracteristicas/modificar")
     public void modificarCaracteristicaQuery( @RequestBody Caracteristicas caracteristicas){
         impl.ModificarCaracteristica(caracteristicas.getId(), caracteristicas.getNombreCaracteristica(), caracteristicas.getValorCaracteristica());
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PutMapping("/caracteristicas/modificar/nombre")
     public void modificarCaracteristicaNombreQuery( @RequestBody Caracteristicas caracteristicas){
         impl.ModificarCaracteristicaNombre(caracteristicas.getId(), caracteristicas.getNombreCaracteristica());
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PutMapping("/caracteristicas/modificar/valor")
     public void modificarCaracteristicaValorQuery( @RequestBody Caracteristicas caracteristicas){
         impl.ModificarCaracteristicaValor(caracteristicas.getId(), caracteristicas.getValorCaracteristica());
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @DeleteMapping("/caracteristicas/eliminar")
     public void eliminarCaracteristicaQuery( @RequestBody Caracteristicas caracteristica){
         impl.EliminarCaracteristica(caracteristica.getId());
