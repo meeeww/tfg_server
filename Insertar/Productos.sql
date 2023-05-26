acb9fe278120526eb8725b0fb3004690c6423c52
diegog24957 <a24957@svalero.com>
2023-05-26 12:46:04 +0200

    Inserts productos

---
+++ Insertar/Productos.sql
@@ -0,0 +1,69 @@
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Coffee', 'Freshly ground coffee beans, hot water, and nothing else.', 'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG', 3.50, 50, 1, 3, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Coffee With Milk', 'Smooth and creamy espresso mixed with steamed milk.', 'https://instafitness.in/wp-content/uploads/2021/03/Almond-Milk-Coffee-600x300.png', 4.75, 50, 1, 4, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Espresso With A Dash Of Milk', 'Espresso with a small amount of hot milk.', 'https://lh3.googleusercontent.com/Tx2DvCmLk3LNOOs6TAl4cTOP1dBETSIo_8W1LdrJ9aNadZrgiXyDKwK6arTAV45X1_0wJvguypwGoI5Jk9KaL0DEiJQNQXu4_wtm8HV48vp96GzaB0FEN-MvTX5m4M2PjeTz4xkH', 3.20, 50, 1, 3, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Frappé', 'Instant coffee whipped with ice, milk and sugar.', 'https://www.monin.com/media/catalog/product/cache/086f110b92ec89cebc323a4780ab42bd/8/1/81a31f41-5281-40c2-9a31-3301000e55cd-2_1.png', 5.70, 50, 1, 4, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Irish Coffee', 'Coffee, Irish whiskey, sugar and whipped cream.', 'https://bakeitwithlove.com/wp-content/uploads/2020/03/Irish-Coffee-sq-1.jpg', 7.20, 50, 1, 4, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Spanish Latte', 'Coffee with condensed milk and a pinch of cinnamon.', 'https://beanthirst.com/wp-content/uploads/2022/12/Spanish-latte-1.jpg', 5.10, 50, 1, 3, 0);
+
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Orange Juice', 'Squeezed from fresh oranges, refreshing and vitamin.', 'https://www.earthfoodandfire.com/wp-content/uploads/2018/04/Homemade-Orange-Juice-500x375.jpg', 4.20, 50, 5, 4, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Pineapple Juice', 'Tropical juice of ripe pineapples, an explosion of flavor.', 'https://cookathomemom.com/wp-content/uploads/2022/12/Pineapple-Ginger-Juice.jpg', 5.70, 50, 5, 4, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Strawberry Juice', 'Vibrant juice of fresh strawberries, sweet and delicious.', 'https://cdn3.foodviva.com/static-content/food-images/juice-recipes/strawberry-juice-recipe/strawberry-juice-recipe.jpg', 4.30, 50, 5, 3, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Grape Juice', 'Ripe grape juice, balance between sweetness and freshness.', 'https://simplelivingrecipes.com/wp-content/uploads/2023/02/grape-juice-recipe-s.jpg', 3.80, 50, 5, 3, 0);
+
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Coffee Slushie', 'Strong coffee shaken with ice and sweetened to taste.', 'https://ikneadtoeat.com/wp-content/uploads/2022/10/coffee-slushie-recipe-4.jpg', 5.60, 50, 6, 3, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Vanilla Slushie', 'Coffee with milk, vanilla essence, ice and whipped cream.', 'https://ucarecdn.com/f93753f5-6b34-4284-aa51-a4e5c8e38750/', 5.40, 50, 6, 4, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Strawberry Slushie', 'Strawberry puree, crushed ice and strawberry syrup.', 'https://res.cloudinary.com/sonic-drive-in/image/upload/c_fit,w_600,h_600,dpr_2,f_auto,q_auto/v1603472032/oa_menu/products/menuproduct-Strawberry_Slush.png', 4.70, 50, 6, 3, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Watermelon  Slushie', 'Fresh watermelon, crushed ice and watermelon syrup.', 'https://www.acouplecooks.com/wp-content/uploads/2021/07/Watermelon-Slushie-004.jpg', 4.60, 50, 6, 3, 0);
+
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Club Sandwich', 'Layers of chicken, bacon, lettuce, tomato and mayonnaise on toasted bread.', 'https://www.unileverfoodsolutions.es/dam/global-ufs/mcos/SPAIN/calcmenu/recipes/ES-recipes/general/club-s%C3%A1ndwich-con-nopollo/main-header.jpg', 10.50, 50, 7, 4, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('BLT', 'Crispy bacon, lettuce, tomato and mayonnaise on toast.', 'https://imag.bonviveur.com/sandwich-blt.jpg', 9.20, 50, 7, 3, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Turkey Club', 'Turkey, bacon, lettuce, tomato and mayonnaise on toast.', 'https://img.taste.com.au/a0minmNN/taste/2016/11/classic-turkey-club-sandwich-24417-1.jpeg', 9.80, 50, 7, 4, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Caprese Sandwich', 'Fresh mozzarella, tomato, basil and olive oil on ciabatta bread.', 'https://cookieandkate.com/images/2020/10/vegetarian-baguette-sandwich-recipe-1.jpg', 9.70, 50, 7, 3, 0);
+
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Butter Croissant', 'Classic butter croissant, puff pastry and golden brown.', 'https://atrianbakers.com/wp-content/uploads/2018/12/01404-1.jpg', 3.80, 50, 8, 4, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Glazed Donut', 'Fluffy donut with sweet colorful icing.', 'https://comercialalias.com/1050/donuts-azucar.jpg', 3.00, 50, 8, 3, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Blueberry Muffin', 'Tender muffin with juicy blueberries and a crunchy topping.', 'https://es.cravingsjournal.com/wp-content/uploads/2021/06/muffins-de-ara%CC%81ndanos-XL-10.jpg', 4.20, 50, 8, 4, 0);
+
+INSERT INTO PRODUCTOS (NOMBRE_PRODUCTO, DESCRIPCION_PRODUCTO, FOTO_PRODUCTO, COSTE_BASE, STOCK, ID_CATEGORIA, ID_CARACTERISTICA, VENTAS)
+VALUES ('Apple pie', 'Delicious crunchy apple pie covered in crumbs.', 'https://www.inspiredtaste.net/wp-content/uploads/2022/11/Apple-Pie-Recipe-Video.jpg', 5.50, 50, 8, 3, 0);
